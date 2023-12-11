f4d1ef744f47dfbf6e89109081aad7d309df3818
Shloon <shlomo.shmouely@gmail.com>
2023-12-11 13:54:49 +0200

    feat: add brotli compression utility

---
+++ Packages/ac.arl.eventhorizon/Runtime/BrotliUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace EventHorizon
+{
+	public class BrotliUtils
+	{
+		public static void Compress(Stream source, Stream destination)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (destination == null) throw new ArgumentNullException(nameof(destination));
+
+			source.Position = 0; // move head back
+			using BrotliStream compressor = new BrotliStream(destination, CompressionLevel.Optimal, leaveOpen: true);
+			source.CopyTo(compressor);
+		}
+
+		public static void Decompress(Stream source, Stream destination)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (destination == null) throw new ArgumentNullException(nameof(destination));
+
+			source.Position = 0; // move head back
+			using BrotliStream decompressor = new BrotliStream(source, CompressionMode.Decompress, leaveOpen: true);
+			decompressor.CopyTo(destination);
+		}
+	}
+}
