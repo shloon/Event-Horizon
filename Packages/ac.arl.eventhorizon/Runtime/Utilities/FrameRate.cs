0bb8c44bb365ab87c7b8d4059866b71dd6916ea4
Shloon <shlomo.shmouely@gmail.com>
2023-12-12 18:55:49 +0200

    fix: add parsing functionality to FrameRate

--- Packages/ac.arl.eventhorizon/Runtime/Utilities/FrameRate.cs
+++ Packages/ac.arl.eventhorizon/Runtime/Utilities/FrameRate.cs
@@ -19,7 +19,7 @@ namespace EventHorizon
 		}
 
 		public readonly double GetAsDouble() => (numerator / (double)denominator);
-		public readonly double GetFrameDuration() => (denominator / (double) numerator);
+		public readonly double GetFrameDuration() => (denominator / (double)numerator);
 
 		public bool Equals(FrameRate other) => numerator == other.numerator && denominator == other.denominator;
 		public override bool Equals(object obj) => obj is FrameRate other && Equals(other);
@@ -28,5 +28,71 @@ namespace EventHorizon
 
 		public static bool operator ==(FrameRate left, FrameRate right) => left.Equals(right);
 		public static bool operator !=(FrameRate left, FrameRate right) => !left.Equals(right);
+
+		public FrameRate Simplify()
+		{
+			var (a, b) = (numerator, denominator);
+			while (b > 0)
+				(a, b) = (b, a % b);
+			var gcd = a;
+			
+			return new FrameRate { denominator = denominator / gcd, numerator = numerator / gcd };
+		}
+
+		public static bool TryParse(string input, out FrameRate output)
+		{
+			input = input?.Trim();
+			if (string.IsNullOrEmpty(input))
+			{
+				output.numerator = -1;
+				output.denominator = -1;
+				return false;
+			}
+
+			var indexOfSlash = input.IndexOf('/');
+
+			if (indexOfSlash != -1)
+			{
+				// if a fraction, try parsing it as a fraction
+				var parts = input.Split('/', 2);
+				var numStr = parts[0];
+				var denStr = parts[1];
+				if (uint.TryParse(denStr, out var den) && uint.TryParse(numStr, out var num) && num > 0 && den > 0)
+				{
+					output.numerator = (int)num;
+					output.denominator = (int)den;
+					output = output.Simplify();
+
+					return true;
+				}
+			}
+			else
+			{
+				// if not a fraction, try parsing as an int or approximate a double
+				if (int.TryParse(input, out var intValue) && intValue > 0)
+				{
+					output.numerator = intValue;
+					output.denominator = 1;
+					return true;
+				}
+
+				if (decimal.TryParse(input, out var decimalValue) && decimalValue > 0)
+				{
+					var parts = input.Split('.');
+					var integerPart = parts[0];
+					var fractionalPart = parts[1];
+
+					output.numerator = int.Parse(integerPart + fractionalPart);
+					output.denominator = (int)Math.Pow(10, fractionalPart.Length);
+					output = output.Simplify();
+
+					return true;
+				}
+			}
+
+			output.numerator = -1;
+			output.denominator = -1;
+			return false;
+		}
 	}
 }
--- Packages/ac.arl.eventhorizon/Tests/Runtime/Utilities/FrameRateTests.cs
+++ Packages/ac.arl.eventhorizon/Tests/Runtime/Utilities/FrameRateTests.cs
@@ -133,7 +133,7 @@ namespace EventHorizon.Tests
 		}
 
 		[Test]
-		public void TrackableID_SerializesAndDeserializesCorrectly()
+		public void SerializesAndDeserializesCorrectly()
 		{
 			FrameRate original = new FrameRate(30, 1);
 			var json = JsonUtility.ToJson(original);
@@ -142,6 +142,76 @@ namespace EventHorizon.Tests
 
 			// Assert
 			Assert.AreEqual(original, deserialized);
+		}
+
+		[TestCase(100,100, 1, 1)]
+		[TestCase(1200,1000, 6, 5)]
+		[TestCase(100,200, 1, 2)]
+		[TestCase(50,100, 1, 2)]
+		[TestCase(75,300, 1, 4)]
+		[TestCase(360,60, 6, 1)]
+		[TestCase(500,500, 1, 1)]
+		[TestCase(144,12, 12, 1)]
+		public void Simplify_CorrectlySimplifiesFraction(int beforeNum, int beforeDen, int expectedNumerator, int expectedDenominator)
+		{
+			FrameRate original = new FrameRate() { numerator = beforeNum, denominator = beforeDen };
+			FrameRate reduced = original.Simplify();
+			Assert.AreEqual(expectedNumerator, reduced.numerator);
+			Assert.AreEqual(expectedDenominator, reduced.denominator);
+		}
+
+		[TestCase("30", 30, 1)]
+		[TestCase("29.97", 2997, 100)]
+		[TestCase("12345.6789", 123456789, 10000)]
+		[TestCase("1.0", 1, 1)]
+		[TestCase("0.001", 1, 1000)]
+		[TestCase("1000000", 1000000, 1)]
+		[TestCase("0.0000001", 1, 10000000)]
+		[TestCase(" 30 ", 30, 1)]
+		[TestCase("30000/1001", 30000, 1001)]
+		[TestCase("100/100", 1, 1)]
+		[TestCase("1200/1000", 6, 5)]
+		[TestCase("100/200", 1, 2)]
+		[TestCase("50/100", 1, 2)]
+		[TestCase("75/300", 1, 4)]
+		[TestCase("360/60", 6, 1)]
+		[TestCase("500/500", 1, 1)]
+		[TestCase("144/12", 12, 1)]
+		public void TryParse_WithProperInput_ReturnsValidFrameRate(string input, int expectedNumerator, int expectedDenominator)
+		{
+			var result = FrameRate.TryParse(input, out FrameRate frameRate);
+
+			Assert.AreEqual(true, result);
+			Assert.AreEqual(expectedNumerator, frameRate.numerator);
+			Assert.AreEqual(expectedDenominator, frameRate.denominator);
+		}
+
+		[TestCase("0")]
+		[TestCase("abc")]
+		[TestCase("0/0")]
+		[TestCase("-1/-3")]
+		[TestCase("-1/3")]
+		[TestCase("1/2/3/4")]
+		[TestCase("")]
+		[TestCase(null)]
+		[TestCase("1/0")]
+		[TestCase("/")]
+		[TestCase("1./2")]
+		[TestCase("1.2/3")]
+		[TestCase("1/3.4")]
+		[TestCase("1/2/3")]
+		[TestCase("30fps")]
+		[TestCase("one/three")]
+		[TestCase("-30")]
+		[TestCase("-29.97")]
+		[TestCase("0/100")]
+		public void TryParse_WithoutProperInput_ReturnsInvalidFrameRate(string input)
+		{
+			var result = FrameRate.TryParse(input, out FrameRate frameRate);
+
+			Assert.AreEqual(false, result);
+			Assert.AreEqual(-1, frameRate.numerator);
+			Assert.AreEqual(-1, frameRate.denominator);
 		}
 	}
 }
