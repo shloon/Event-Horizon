585c8d59d06af9a7b0f03f2d1f09424ca4157188
Shloon <shlomo.shmouely@gmail.com>
2023-12-18 00:20:11 +0200

    test: move trackable creation utilities outside of test class

--- Packages/ac.arl.eventhorizon/Tests/Runtime/TrackableManagerTest.cs
+++ Packages/ac.arl.eventhorizon/Tests/Runtime/TrackableManagerTest.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
+using EventHorizon.Tests.Utilities;
+
 namespace EventHorizon.Tests
 {
 	public class TrackableManagerTest
@@ -13,100 +15,100 @@ namespace EventHorizon.Tests
 		[Test]
 		public void Register_WithValidTrackable_ShouldAddTrackable()
 		{
-			var trackable = CreateTrackable(new TrackableID(1));
+			var trackable = TrackableTestUtils.CreateTrackable(new TrackableID(1));
 			var manager = new TrackableManager();
 
 			Assert.DoesNotThrow(() => manager.Register(trackable));
 
-			DestroyTrackable(trackable);
+			TrackableTestUtils.DestroyTrackable(trackable);
 		}
 
 		[Test]
 		public void Register_WithInvalidId_ShouldThrowArgumentException()
 		{
-			var trackable = CreateTrackable();
+			var trackable = TrackableTestUtils.CreateTrackable();
 			var manager = new TrackableManager();
 
 			Assert.Throws<ArgumentException>(() => manager.Register(trackable));
 
-			DestroyTrackable(trackable);
+			TrackableTestUtils.DestroyTrackable(trackable);
 		}
 
 		[Test]
 		public void Register_AlreadyRegisteredTrackable_ShouldThrowInvalidOperationException()
 		{
-			var trackable = CreateTrackable(new TrackableID(1));
+			var trackable = TrackableTestUtils.CreateTrackable(new TrackableID(1));
 			var manager = new TrackableManager();
 
 			manager.Register(trackable);
 
 			Assert.Throws<InvalidOperationException>(() => manager.Register(trackable));
 
-			DestroyTrackable(trackable);
+			TrackableTestUtils.DestroyTrackable(trackable);
 		}
 
 		[Test]
 		public void Register_DifferentTrackableWithSameId_ShouldThrowInvalidOperationException()
 		{
-			var trackable1 = CreateTrackable(new TrackableID(1));
-			var trackable2 = CreateTrackable(new TrackableID(1));
+			var trackable1 = TrackableTestUtils.CreateTrackable(new TrackableID(1));
+			var trackable2 = TrackableTestUtils.CreateTrackable(new TrackableID(1));
 			var manager = new TrackableManager();
 
 			manager.Register(trackable1);
 
 			Assert.Throws<InvalidOperationException>(() => manager.Register(trackable2));
 
-			DestroyTrackable(trackable1);
-			DestroyTrackable(trackable2);
+			TrackableTestUtils.DestroyTrackable(trackable1);
+			TrackableTestUtils.DestroyTrackable(trackable2);
 		}
 
 		[Test]
 		public void Unregister_RegisteredTrackable_ShouldRemoveTrackable()
 		{
-			var trackable = CreateTrackable(new TrackableID(1));
+			var trackable = TrackableTestUtils.CreateTrackable(new TrackableID(1));
 			var manager = new TrackableManager();
 			manager.Register(trackable);
 
 			Assert.DoesNotThrow(() => manager.Unregister(trackable));
 
-			DestroyTrackable(trackable);
+			TrackableTestUtils.DestroyTrackable(trackable);
 		}
 
 		[Test]
 		public void Unregister_WithInvalidId_ShouldThrowArgumentException()
 		{
-			var trackable = CreateTrackable();
+			var trackable = TrackableTestUtils.CreateTrackable();
 			var manager = new TrackableManager();
 
 			Assert.Throws<ArgumentException>(() => manager.Unregister(trackable));
 
-			DestroyTrackable(trackable);
+			TrackableTestUtils.DestroyTrackable(trackable);
 		}
 
 		[Test]
 		public void Unregister_UnregisteredTrackable_ShouldThrowInvalidOperationException()
 		{
-			var trackable = CreateTrackable(new TrackableID(1));
+			var trackable = TrackableTestUtils.CreateTrackable(new TrackableID(1));
 			var manager = new TrackableManager();
 
 			Assert.Throws<InvalidOperationException>(() => manager.Unregister(trackable));
 
-			DestroyTrackable(trackable);
+			TrackableTestUtils.DestroyTrackable(trackable);
 		}
 
 		[Test]
 		public void Unregister_DifferentTrackableWithSameRegisteredId_ShouldThrowInvalidOperationException()
 		{
-			var trackable1 = CreateTrackable(new TrackableID(1));
-			var trackable2 = CreateTrackable(new TrackableID(1));
+			var trackable1 = TrackableTestUtils.CreateTrackable(new TrackableID(1));
+			var trackable2 = TrackableTestUtils.CreateTrackable(new TrackableID(1));
 			var manager = new TrackableManager();
 
 			manager.Register(trackable1);
 
 			Assert.Throws<InvalidOperationException>(() => manager.Unregister(trackable2));
 
-			DestroyTrackable(trackable1);
-			DestroyTrackable(trackable2);
+			TrackableTestUtils.DestroyTrackable(trackable1);
+			TrackableTestUtils.DestroyTrackable(trackable2);
 		}
 
 		[Test]
@@ -124,13 +126,13 @@ namespace EventHorizon.Tests
 		{
 			var rng = new PcgRng(1023);
 			var trackableManager = new TrackableManager(rng);
-			var trackable = CreateTrackable(new TrackableID(10));
+			var trackable = TrackableTestUtils.CreateTrackable(new TrackableID(10));
 			trackableManager.Register(trackable);
 
 			var newId = trackableManager.GenerateId();
 			Assert.AreNotEqual(trackable.id, newId);
 
-			DestroyTrackable(trackable);
+			TrackableTestUtils.DestroyTrackable(trackable);
 		}
 
 		[Test]
@@ -141,12 +143,12 @@ namespace EventHorizon.Tests
 			mockRng.Setup(rng => rng.Next()).Returns(constId);
 
 			var manager = new TrackableManager(mockRng.Object);
-			var trackable = CreateTrackable(new TrackableID(constId));
+			var trackable = TrackableTestUtils.CreateTrackable(new TrackableID(constId));
 			manager.Register(trackable);
 
 			Assert.Throws<InvalidOperationException>(() => manager.GenerateId());
 
-			DestroyTrackable(trackable);
+			TrackableTestUtils.DestroyTrackable(trackable);
 		}
 
 		[Test]
@@ -157,8 +159,8 @@ namespace EventHorizon.Tests
 			mockRng.Setup(rng => rng.Next()).Returns(trackable1Id);
 
 			var trackableManager = new TrackableManager(mockRng.Object);
-			var trackable1 = CreateTrackable(new TrackableID(trackable1Id));
-			var trackable2 = CreateTrackable(new TrackableID(trackable1Id + 1));
+			var trackable1 = TrackableTestUtils.CreateTrackable(new TrackableID(trackable1Id));
+			var trackable2 = TrackableTestUtils.CreateTrackable(new TrackableID(trackable1Id + 1));
 
 			trackableManager.Register(trackable1);
 			trackableManager.Register(trackable2);
@@ -192,41 +194,14 @@ namespace EventHorizon.Tests
 			// Register max-1 trackables
 			for (var i = 0; i < TrackableManager.MaxGenerateAttempts - 1; i++)
 			{
-				var trackable = CreateTrackable(new TrackableID((uint) i + 1));
+				var trackable = TrackableTestUtils.CreateTrackable(new TrackableID((uint) i + 1));
 				trackableManager.Register(trackable);
-				DestroyTrackable(trackable);
+				TrackableTestUtils.DestroyTrackable(trackable);
 			}
 
 			var newId = trackableManager.GenerateId();
 			Assert.IsTrue(newId.IsValid);
 			Assert.IsFalse(trackableManager.RegisteredTrackables.ContainsKey(newId));
 		}
-
-		#region Helpers
-
-		private class DummyManager : ITrackableManager
-		{
-			public IReadOnlyDictionary<TrackableID, Trackable> RegisteredTrackables => null;
-			public void Register(Trackable trackable) { }
-			public void Unregister(Trackable trackable) { }
-			public TrackableID GenerateId() => TrackableID.Unassigned;
-		}
-
-		private Trackable CreateTrackable(TrackableID id = new())
-		{
-			var go = new GameObject("Trackable");
-			go.SetActive(false);
-
-			var trackable = go.AddComponent<Trackable>();
-			trackable.manager = new DummyManager();
-			trackable.id = id;
-			go.SetActive(true);
-
-			return trackable;
-		}
-
-		private void DestroyTrackable(Trackable trackable) => Object.Destroy(trackable.gameObject);
-
-		#endregion
 	}
 }
