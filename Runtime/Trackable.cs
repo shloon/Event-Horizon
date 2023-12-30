7b7af2d12e6b881fda5ad210814a33d04031de60
Shloon <shlomo.shmouely@gmail.com>
2023-12-30 17:30:50 +0200

    fix: add component menu for Trackable

--- Runtime/Trackable.cs
+++ Runtime/Trackable.cs
@@ -5,12 +5,18 @@ namespace EventHorizon
 {
 	[DisallowMultipleComponent]
 	[DefaultExecutionOrder(-99)]
+	[AddComponentMenu("Event Horizon/Trackable")]
 	public sealed class Trackable : MonoBehaviour
 	{
 		[SerializeField] public TrackableID id;
 		[NonSerialized] public ITrackableManager manager;
 
-		private void Awake() { manager = manager ?? TrackableManagerComponent.Instance; manager?.Register(this); }
+		private void Awake()
+		{
+			manager = manager ?? TrackableManagerComponent.Instance;
+			manager?.Register(this);
+		}
+
 		private void OnDestroy() => manager?.Unregister(this);
 	}
 }
