457bd3d0a2d0491e1e70e58386b11b2ed8d98cc2
Shloon <shlomo.shmouely@gmail.com>
2023-12-30 16:19:26 +0200

    chore: add code coverage ignore

--- Editor/Recordings/RecordingImporterEditor.cs
+++ Editor/Recordings/RecordingImporterEditor.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using UnityEditor;
 using UnityEditor.AssetImporters;
 
@@ -6,8 +7,10 @@ namespace EventHorizon.Editor
 	[CustomEditor(typeof(RecordingImporter))]
 	public class RecordingImporterEditor : ScriptedImporterEditor
 	{
+		[ExcludeFromCodeCoverage] // unity internal function, cannot really test this
 		public override void OnInspectorGUI()
 		{
+			// This removes the "Script" field from the recording importer
 			ApplyRevertGUI();
 		}
 	}
