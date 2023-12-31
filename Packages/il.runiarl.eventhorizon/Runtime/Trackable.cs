af996b86e7d62cac99339a6cc2166a2bfb17f54c
Shloon <shlomo.shmouely@gmail.com>
2023-12-31 11:57:49 +0200

    fix: always execute trackable

--- Packages/il.runiarl.eventhorizon/Runtime/Trackable.cs
+++ Packages/il.runiarl.eventhorizon/Runtime/Trackable.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor;
 using UnityEngine;
 
 namespace EventHorizon
@@ -6,6 +7,7 @@ namespace EventHorizon
 	[DisallowMultipleComponent]
 	[DefaultExecutionOrder(-99)]
 	[AddComponentMenu("Event Horizon/Trackable")]
+	[ExecuteAlways]
 	public sealed class Trackable : MonoBehaviour
 	{
 		[SerializeField] public TrackableID id;
@@ -14,6 +16,16 @@ namespace EventHorizon
 		private void Awake()
 		{
 			manager = manager ?? TrackableManagerComponent.Instance;
+#if UNITY_EDITOR
+			// Generate key if in Edit Mode
+			if (manager != null && !EditorApplication.isPlayingOrWillChangePlaymode)
+			{
+				if (!id.IsValid)
+				{
+					id = manager.GenerateId();
+				}
+			}
+#endif
 			manager?.Register(this);
 		}
 
