b5facc84dd6cec714e5ec2cb272c7188d6e18808
Shloon <shlomo.shmouely@gmail.com>
2023-12-31 23:09:42 +0200

    fix: TrackableTestUtils.DestroyTrackable to use DestroyImmediate in Edit Mode

--- Packages/il.runiarl.eventhorizon/Tests/Runtime/TrackableTestUtils.cs
+++ Packages/il.runiarl.eventhorizon/Tests/Runtime/TrackableTestUtils.cs
@@ -5,16 +5,6 @@ namespace EventHorizon.Tests.Utilities
 {
 	public static class TrackableTestUtils
 	{
-		internal class DummyManager : ITrackableManager
-		{
-			public IReadOnlyDictionary<TrackableID, Trackable> RegisteredTrackables => null;
-			public void Register(Trackable trackable) { }
-			public void Unregister(Trackable trackable) { }
-			public void ChangeTrackableID(TrackableID previousID, TrackableID newID) {}
-
-			public TrackableID GenerateId() => TrackableID.Unassigned;
-		}
-
 		public static Trackable CreateTrackable(TrackableID id = new())
 		{
 			var go = new GameObject("Trackable");
@@ -28,6 +18,23 @@ namespace EventHorizon.Tests.Utilities
 			return trackable;
 		}
 
-		public static void DestroyTrackable(Trackable trackable) => Object.Destroy(trackable.gameObject);
+		public static void DestroyTrackable(Trackable trackable)
+		{
+#if UNITY_EDITOR
+			Object.DestroyImmediate(trackable.gameObject);
+#else
+			Object.Destroy(trackable.gameObject);
+#endif
+		}
+
+		internal class DummyManager : ITrackableManager
+		{
+			public IReadOnlyDictionary<TrackableID, Trackable> RegisteredTrackables => null;
+			public void Register(Trackable trackable) { }
+			public void Unregister(Trackable trackable) { }
+			public void ChangeTrackableID(TrackableID previousID, TrackableID newID) { }
+
+			public TrackableID GenerateId() => TrackableID.Unassigned;
+		}
 	}
 }
