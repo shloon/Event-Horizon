8ed49176bc035f9c57af6f22d5faee50bc897325
Shloon <shlomo.shmouely@gmail.com>
2024-01-02 13:59:53 +0200

    refactor: apply rider autofixes

--- Packages/il.runiarl.eventhorizon/Runtime/TrackableID.cs
+++ Packages/il.runiarl.eventhorizon/Runtime/TrackableID.cs
@@ -6,6 +6,7 @@ namespace EventHorizon
 	[Serializable]
 	public struct TrackableID : IEquatable<TrackableID>, IComparable<TrackableID>
 	{
+		public static readonly TrackableID Unassigned = new();
 
 		// unity serialization requires this to be a non-readonly field
 #pragma warning disable IDE0044
@@ -13,17 +14,16 @@ namespace EventHorizon
 #pragma warning restore IDE0044
 
 		public TrackableID(uint id = 0) => @internal = id;
-		public static TrackableID Unassigned = new TrackableID();
 
 		public uint Internal => @internal;
-		public bool IsValid => this.@internal != 0;
-
-		public override string ToString() => $"TrackableId({@internal.ToString()})";
-		public bool Equals(TrackableID other) => @internal == other.@internal;
-		public override bool Equals(object obj) => obj is TrackableID other && Equals(other);
-		public override int GetHashCode() => (int) @internal;
+		public bool IsValid => @internal != 0;
 
 		public int CompareTo(TrackableID other) => @internal.CompareTo(other.@internal);
+		public bool Equals(TrackableID other) => @internal == other.@internal;
+
+		public override string ToString() => $"TrackableId({@internal.ToString()})";
+		public override bool Equals(object obj) => obj is TrackableID other && Equals(other);
+		public override int GetHashCode() => (int) @internal;
 		public static bool operator ==(TrackableID one, TrackableID other) => one.@internal == other.@internal;
 		public static bool operator !=(TrackableID one, TrackableID other) => !(one == other);
 	}
