8140029c55197baafc25f7db213b6e8332ff2552
Shloon <shlomo.shmouely@gmail.com>
2024-01-03 11:26:39 +0200

    refactor: move common UI elements a common class

---
+++ Packages/il.runiarl.eventhorizon/Editor/UIHelpers.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.UIElements;
+
+namespace EventHorizon
+{
+	public static class UIHelpers
+	{
+		public static VisualElement CreateHeader(string headerText)
+		{
+			var decoratorContainer = new VisualElement();
+			decoratorContainer.AddToClassList("unity-decorator-drawers-container");
+
+			var metadataLabel = new Label(headerText);
+			metadataLabel.AddToClassList("unity-header-drawer__label");
+			decoratorContainer.Add(metadataLabel);
+
+			return decoratorContainer;
+		}
+	}
+}
