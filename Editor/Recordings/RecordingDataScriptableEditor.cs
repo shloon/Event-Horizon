526832ff99bb03491e9c3be60d3ff49f140ba27d
Shloon <shlomo.shmouely@gmail.com>
2024-01-03 11:26:39 +0200

    refactor: move common UI elements a common class

--- Editor/Recordings/RecordingDataScriptableEditor.cs
+++ Editor/Recordings/RecordingDataScriptableEditor.cs
@@ -1,6 +1,5 @@
 using UnityEditor;
 using UnityEditor.UIElements;
-using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace EventHorizon.Editor
@@ -8,21 +7,9 @@ namespace EventHorizon.Editor
 	[CustomEditor(typeof(RecordingDataScriptable))]
 	public class RecordingDataScriptableEditor : UnityEditor.Editor
 	{
-		public VisualElement CreateHeader(string headerText)
-		{
-			var decoratorContainer = new VisualElement();
-			decoratorContainer.AddToClassList("unity-decorator-drawers-container");
-
-			var metadataLabel = new Label(headerText);
-			metadataLabel.AddToClassList("unity-header-drawer__label");
-			decoratorContainer.Add(metadataLabel);
-
-			return decoratorContainer;
-		}
-
 		public override VisualElement CreateInspectorGUI()
 		{
-			RecordingDataScriptable scriptable = (RecordingDataScriptable) target;
+			var scriptable = (RecordingDataScriptable) target;
 			var container = new VisualElement();
 
 			var playButton = new Button();
@@ -30,14 +17,12 @@ namespace EventHorizon.Editor
 			playButton.clicked += () => { EventHorizonInspectionStateToggler.TogglePlayModeAndSetupCleanup(); };
 			container.Add(playButton);
 
-			var metadataHeader = CreateHeader("Metadata");
+			var metadataHeader = UIHelpers.CreateHeader("Metadata");
 			container.Add(metadataHeader);
 
 			var sceneNameField = new TextField
 			{
-				label = "Scene Name",
-				isReadOnly = true,
-				value = scriptable.data.metadata.sceneName
+				label = "Scene Name", isReadOnly = true, value = scriptable.data.metadata.sceneName
 			};
 			sceneNameField.AddToClassList("unity-base-field__aligned");
 			container.Add(sceneNameField);
@@ -45,23 +30,19 @@ namespace EventHorizon.Editor
 			var fpsProperty = serializedObject.FindProperty("data.metadata.fps");
 			var fpsField = new TextField
 			{
-				label = "Frame Rate",
-				isReadOnly = true,
-				value = scriptable.data.metadata.fps.ToString()
+				label = "Frame Rate", isReadOnly = true, value = scriptable.data.metadata.fps.ToString()
 			};
 			fpsField.AddToClassList("unity-base-field__aligned");
 			container.Add(fpsField);
 
 			var framesLengthField = new IntegerField
 			{
-				label = "Number of Frames",
-				value = scriptable.data.frames.Length,
-				isReadOnly = true
+				label = "Number of Frames", value = scriptable.data.frames.Length, isReadOnly = true
 			};
 			framesLengthField.AddToClassList("unity-base-field__aligned");
 			container.Add(framesLengthField);
 
-			var rawDataHeader = CreateHeader("Raw Data");
+			var rawDataHeader = UIHelpers.CreateHeader("Raw Data");
 			container.Add(rawDataHeader);
 
 			var frameField = new PropertyField(serializedObject.FindProperty("data.frames"));
