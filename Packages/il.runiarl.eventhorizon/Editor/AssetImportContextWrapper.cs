b26e10f91adc9aafaf5ad234bec91ba465583d57
Shloon <shlomo.shmouely@gmail.com>
2024-01-05 22:31:42 +0200

    chore: delete files related to old format

---
+++ Packages/il.runiarl.eventhorizon/Editor/AssetImportContextWrapper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor.AssetImporters;
+using UnityEngine;
+
+namespace EventHorizon.Editor
+{
+	public interface IAssetImportContext
+	{
+		void AddObjectToAsset(string identifier, Object objectToAdd);
+		void SetMainObject(Object mainObject);
+	}
+
+	internal class AssetImportContextWrapper : IAssetImportContext
+	{
+		private readonly AssetImportContext ctx;
+		public AssetImportContextWrapper(AssetImportContext context) => ctx = context;
+
+		public void AddObjectToAsset(string identifier, Object objectToAdd) =>
+			ctx.AddObjectToAsset(identifier, objectToAdd);
+
+		public void SetMainObject(Object mainObject) => ctx.SetMainObject(mainObject);
+	}
+}
--- Packages/il.runiarl.eventhorizon/Runtime/RecordingFormat.cs
+++ Packages/il.runiarl.eventhorizon/Runtime/RecordingFormat.cs
@@ -3,46 +3,5 @@ using UnityEngine;
 
 namespace EventHorizon
 {
-	// Internal representation of the format.
-	// Serialized using unity's JsonUtility API
-	// which we compress later via Brotli (see `Recording.WrapStream`)
-	[Serializable]
-	public class RecordingData
-	{
-		public RecordingFormatVersion version;
-		public RecordingMetadata metadata;
-		public RecordingFrameData[] frames;
-	}
-
 	public enum RecordingFormatVersion { V1, V2, Current = V1 }
-
-	[Serializable]
-	public struct RecordingMetadata
-	{
-		public string sceneName;
-		public FrameRate fps;
-	}
-
-	[Serializable]
-	public partial struct RecordingFrameData
-	{
-		public int frame;
-		public double timeCode;
-		public RecordingTrackerData[] trackers;
-	}
-
-	[Serializable]
-	public struct RecordingTrackerData
-	{
-		public TrackableID id;
-		public TransformData transform;
-	}
-
-	[Serializable]
-	public struct TransformData
-	{
-		public Vector3 position;
-		public Quaternion rotation;
-		public Vector3 scale;
-	}
 }
