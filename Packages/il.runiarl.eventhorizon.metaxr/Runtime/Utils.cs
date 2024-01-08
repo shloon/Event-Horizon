764b329f8fa550531021d4c6f959fd5c41291a12
Shloon <shlomo.shmouely@gmail.com>
2024-01-08 22:32:17 +0200

    refactor: clean up some code regarding activation trackables

--- Packages/il.runiarl.eventhorizon.metaxr/Runtime/Utils.cs
+++ Packages/il.runiarl.eventhorizon.metaxr/Runtime/Utils.cs
@@ -5,18 +5,25 @@ namespace EventHorizon.MetaXR
 {
 	public class Utils
 	{
-		public static T AddTrackable<T, TU>(GameObject targetGameObject, TrackableID id) where T:BaseTrackableComponent<TU> where TU: IPacket {
+		public static T AddTrackable<T, TU>(GameObject targetGameObject, TrackableID id)
+			where T : BaseTrackableComponent<TU> where TU : IPacket
+		{
 			var trackableComponent = targetGameObject.AddComponent<T>();
 			trackableComponent.Id = id;
 			TrackableManagerComponent.Instance.Register(trackableComponent);
 			return trackableComponent;
 		}
-		
-		public static TransformTrackableComponent AddTransformTrackable(GameObject targetGameObject, TrackableID id, bool isLocal = false)
+
+		public static TransformTrackableComponent AddTransformTrackable(GameObject targetGameObject, TrackableID id,
+			bool isLocal = false)
 		{
 			var trackableComponent = AddTrackable<TransformTrackableComponent, TransformPacket>(targetGameObject, id);
 			trackableComponent.isLocal = isLocal;
 			return trackableComponent;
 		}
+
+		public static ActivationTrackableComponent
+			AddActivationTrackable(GameObject targetGameObject, TrackableID id) =>
+			AddTrackable<ActivationTrackableComponent, ActivationPacket>(targetGameObject, id);
 	}
 }
